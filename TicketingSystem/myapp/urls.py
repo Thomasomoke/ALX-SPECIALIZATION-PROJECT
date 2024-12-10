cb1b38d637a5ce96d3ba6bda547d75ab3f75bee7
Thomasomoke <omokethomas644@gmail.com>
2024-12-10 09:43:19 +0300

    project

---
+++ TicketingSystem/myapp/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.registerpage, name="register"),
+    path('login/', views.loginpage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('', views.dashBoard, name="dashboard"),
+    path('products/', views.products, name="products"),
+    path('customer/<str:pk>/', views.customer, name="customer"),
+
+    #------------ (CREATE URLS) ------------
+    path('create_order/', views.createOrder, name="create_order"),
+   
+    #------------ (UPDATE URLS) ------------
+    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
+
+
+    #------------ (UPDATE URLS) ------------
+    path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
+
+]

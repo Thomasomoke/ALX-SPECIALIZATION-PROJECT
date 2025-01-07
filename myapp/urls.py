1f40d90cf809783f7b7884569d77768ed7aa6196
Thomasomoke <omokethomas644@gmail.com>
2025-01-07 10:58:26 +0300

    This is a file containing the API documentation for the API endpoints of the Event Ease project

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,26 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+       openapi.Info(
+           title="Your API Title",
+           default_version='v1',
+           description="API documentation for your Django application",
+           terms_of_service="https://www.google.com/policies/terms/",
+           contact=openapi.Contact(email="contact@yourapi.local"),
+           license=openapi.License(name="BSD License"),
+       ),
+       public=True,
+       permission_classes=(permissions.AllowAny,),
+   )
 
 urlpatterns = [
     path('register/', views.registerpage, name="register"),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('login/', views.loginpage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('', views.loginpage, name="login"),
